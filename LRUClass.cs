ad5b30882bba2f8789d8b0df85ff035eb2df36ca
alirezamires <alirezamires@gmail.com>
2016-11-27 16:36:54 +0330

    Create LRUClass.cs

---
+++ LRUClass.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRU_Cache
+{
+      class LRUClass<V,K>
+    {
+        private int capacity;//Zarfiat
+        private Dictionary<K, LinkedListNode<LRUCacheItem<K, V>>> cacheMap = new Dictionary<K, LinkedListNode<LRUCacheItem<K, V>>>();
+        private LinkedList<LRUCacheItem<K, V>> lruList = new LinkedList<LRUCacheItem<K, V>>();
+         public void LRUCache(int capacity)
+        {
+            this.capacity = capacity;
+        }
+        public V get(K key)
+        {
+            LinkedListNode<LRUCacheItem<K, V>> node;
+            if (cacheMap.TryGetValue(key, out node))
+            {
+                V value = node.Value.value;
+                lruList.Remove(node);
+                lruList.AddLast(node);
+                return value;
+            }
+            return default(V);
+        }
+        public void add(K key, V val)
+        {
+            if (cacheMap.Count >= capacity)
+            {
+                RemoveFirst();
+            }
+
+            LRUCacheItem<K, V> cacheItem = new LRUCacheItem<K, V>(key, val);
+            LinkedListNode<LRUCacheItem<K, V>> node = new LinkedListNode<LRUCacheItem<K, V>>(cacheItem);
+            lruList.AddLast(node);
+            cacheMap.Add(key, node);
+        }
+
+        private void RemoveFirst()
+        {
+            // Remove from LRUPriority
+            LinkedListNode<LRUCacheItem<K,V>> node = lruList.First;
+            lruList.RemoveFirst();
+
+            // Remove from cache
+            cacheMap.Remove(node.Value.key);
+        }
+    }
+
+    class LRUCacheItem<K,V>
+    {
+        public LRUCacheItem(K k, V v)
+        {
+            key = k;
+            value = v;
+        }
+        public K key;
+        public V value;
+    }
+    
+}
